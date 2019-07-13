7b887e2ef8c0a33feb6bd98157e739909cd1f60b
Shameer Kunjumohamed <sameerean@gmail.com>
2019-07-13 13:49:06 +0400

    vagrant-django

--- app/vagrant_django/urls.py
+++ app/vagrant_django/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('polls/', include('polls.urls')),
+    path('vagrant-django/', include('vagrant-django.urls')),
 
 ]
