b2dd9dc382b5563845cd32a70c64b6095e2b1596
Shameer Kunjumohamed <sameerean@gmail.com>
2019-07-13 14:55:06 +0400

    vdjango

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('vagrant-django/', include('vagrant-django.urls')),
+    path('vdjango/', include('vagrant_django.urls')),
     path('admin/', admin.site.urls),
 ]
  
