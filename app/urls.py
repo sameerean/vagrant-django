5fae07592d4a244de20b16d5643e2bdace072e40
Shameer Kunjumohamed <sameerean@gmail.com>
2019-07-13 15:04:14 +0400

    bug fix

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('vdjango/', include('vagrant_django.urls')),
-    path('admin/', admin.site.urls),f
-]
- 
