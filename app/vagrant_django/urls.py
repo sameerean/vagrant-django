c6831b8ef8b8d01bb363c38a6abebc869ce3a6de
Shameer Kunjumohamed <sameerean@gmail.com>
2019-07-13 13:53:43 +0400

    bug fix

--- app/vagrant_django/urls.py
+++ app/vagrant_django/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('vagrant-django/', include('vagrant-django.urls')),
+    # path('vagrant-django/', include('vagrant-django.urls')),
 
 ]
